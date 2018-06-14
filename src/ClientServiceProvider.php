1c361a79916a2715d72572f0cbe9d13d73945d7d
Christophe Vuagniaux <christophe@inetis.ch>
2018-06-14 14:50:15 +0200

    Fix publish config file source path

--- src/ClientServiceProvider.php
+++ src/ClientServiceProvider.php
@@ -16,7 +16,7 @@ class ClientServiceProvider extends ServiceProvider
         $this->loadRoutesFrom(__DIR__ . '/routes.php');
 
         $this->publishes([
-            __DIR__ . '/config/cmspilot.php' => config_path('cmspilot.php'),
+            __DIR__ . '/../config/cmspilot.php' => config_path('cmspilot.php'),
         ]);
     }
 
