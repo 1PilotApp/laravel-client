6bf592e4cb14b58a508e448d5cc564c4e948b74a
Christophe Vuagniaux <christophe@inetis.ch>
2018-06-21 14:33:11 +0200

    Add few comments in config file

--- config/cmspilot.php
+++ config/cmspilot.php
@@ -1,11 +1,30 @@
 <?php
 
-
 return [
+
     /*
-     * cmsPilot will sign validation call using a secret. Please set a strong one
-     */
+    |--------------------------------------------------------------------------
+    | CmsPilot - Private Key
+    |--------------------------------------------------------------------------
+    |
+    | cmsPilot will sign validation call using this secret. Please set
+    | a random generated string.
+    |
+    */
+
     'private_key' => env('CMSPILOT_PRIVATE_KEY'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Disable timestamp verification
+    |--------------------------------------------------------------------------
+    |
+    | Only do that if your server is not at time and you can't fix that
+    |
+    | Set this option to TRUE will considerably reduce the security
+    |
+    */
+
     'skip_time_stamp_validation' => env('CMSPILOT_SKIP_TIME_STAMP_VALIDATION'),
 
 ];
