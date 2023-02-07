0139cde00c284d00a298221a52b0f3f90dd3abc4
Christophe Vuagniaux <christophe@inetis.ch>
2023-02-07 11:33:06 +0100

    MailTester send valid HTML body

--- src/Controllers/MailTesterController.php
+++ src/Controllers/MailTesterController.php
@@ -44,7 +44,7 @@ class MailTesterController extends Controller
         Mail::send([], [], function (\Illuminate\Mail\Message $message) use ($email) {
             $message
                 ->to($email)
-                ->subject('Test send by 1Pilot.io for ensure emails are properly sent');
+                ->subject('Test mail from 1Pilot.io to ensure emails are properly sent');
 
             // Laravel < 9.x SwiftMailer
             if (method_exists($message, 'getSwiftMessage')) {
@@ -55,7 +55,7 @@ class MailTesterController extends Controller
 
             // Laravel >= 9.x Symfony Mailer
             $message->text($this->getBody());
-            $message->html($this->getBody());
+            $message->html($this->getBodyHtml());
         });
     }
 
@@ -117,6 +117,67 @@ Can you...
 
 Space Oddity
 David Bowie
+EOF;
+    }
+
+    protected function getBodyHtml()
+    {
+        $siteUrl = config('app.url');
+
+        return <<<EOF
+<p>This email was automatically sent by the 1Pilot Client installed on $siteUrl.</p>
+
+<p>Ground control to Major Tom<br>
+Ground control to Major Tom<br>
+Take your protein pills and put your helmet on</p>
+
+<p>Ground control to Major Tom<br>
+(10, 9, 8, 7)<br>
+Commencing countdown, engines on<br>
+(6, 5, 4, 3)<br>
+Check ignition, and may God's love be with you<br>
+(2, 1, liftoff)</p>
+
+<p>This is ground control to Major Tom,</p>
+
+<p>You've really made the grade<br>
+And the papers want to know whose shirts you wear<br>
+Now it's time to leave the capsule if you dare</p>
+
+<p>This is Major Tom to ground control<br>
+I'm stepping through the door<br>
+And I'm floating in the most of peculiar way<br>
+And the stars look very different today</p>
+
+<p>For here am I sitting in a tin can<br>
+Far above the world<br>
+Planet Earth is blue, and there's nothing I can do</p>
+
+<p>Though I'm past 100,000 miles<br>
+I'm feeling very still<br>
+And I think my spaceship knows which way to go<br>
+Tell my wife I love her very much, she knows</p>
+
+<p>Ground control to Major Tom,<br>
+Your circuit's dead, there's something wrong<br>
+Can you hear me Major Tom?<br>
+Can you hear me Major Tom?<br>
+Can you hear me Major Tom?<br>
+Can you...</p>
+
+<p>Here am I floating round my tin can<br>
+Far above the moon<br>
+Planet Earth is blue, and there's nothing I can do...</p>
+
+<p>Ground control to Major Tom,<br>
+Your circuit's dead, there's something wrong<br>
+Can you hear me Major Tom?<br>
+Can you hear me Major Tom?<br>
+Can you hear me Major Tom?<br>
+Can you...</p>
+
+<p>Space Oddity<br>
+David Bowie</p>
 EOF;
     }
 }
